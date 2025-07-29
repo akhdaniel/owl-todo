2d5ca76a477a399e884b456d7c2001795212338b
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2025-07-29 20:50:09 +0700

    Clean up: remove all .pyc and .DS_Store files before push

---
+++ vit_todo_inherit/__manifest__.py
@@ -0,0 +1,28 @@
+#-*- coding: utf-8 -*-
+
+{
+	"name": "OWL Todo Inherited",
+	"version": "1.0",
+	"depends": [
+		"base",
+		"vit_todo"
+	],
+	"author": "Akhmad Daniel Sembiring",
+	"category": "Utility",
+	"website": "http://vitraining.com",
+	"images": [
+		"static/description/images/main_screenshot.jpg"
+	],
+	"price": "100",
+	"license": "OPL-1",
+	"currency": "USD",
+	"summary": "",
+	"description": "",
+	"data": [
+		#"view/todo.xml"
+	],
+	"installable": True,
+	"auto_install": False,
+	"application": True,
+	"odooVersion": 18
+}
